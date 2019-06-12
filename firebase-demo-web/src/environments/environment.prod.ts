aac6a20320b13d3bf710cc4fb15576f67b933564
Chiramet (Phong) Penglerd <Phong@hotmail.ch>
2019-06-12 14:25:27 +0200

    version with git

--- firebase-demo-web/src/environments/environment.prod.ts
+++ firebase-demo-web/src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
-import {version} from "../_versions";
+import {version} from '../_versions';
 
 export const environment = {
   production: true,
-  version: version,
+  version,
   firebase: {
-    apiKey: "AIzaSyAJsJNEBcsnj-NvJT65hkW9oct33Y1kcDk",
-    authDomain: "fir-dev-423cd.firebaseapp.com",
-    databaseURL: "https://fir-dev-423cd.firebaseio.com",
-    projectId: "fir-dev-423cd",
-    storageBucket: "fir-dev-423cd.appspot.com",
-    messagingSenderId: "979297538852",
-    appId: "1:979297538852:web:d78f26a4739bb72a"
+    apiKey: 'AIzaSyAJsJNEBcsnj-NvJT65hkW9oct33Y1kcDk',
+    authDomain: 'fir-dev-423cd.firebaseapp.com',
+    databaseURL: 'https://fir-dev-423cd.firebaseio.com',
+    projectId: 'fir-dev-423cd',
+    storageBucket: 'fir-dev-423cd.appspot.com',
+    messagingSenderId: '979297538852',
+    appId: '1:979297538852:web:d78f26a4739bb72a'
   }
 };
--- firebase-demo-web/src/environments/environment.ts
+++ firebase-demo-web/src/environments/environment.ts
@@ -1,19 +1,19 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-import {version} from "../_versions";
+import {versionLong} from '../_versions';
 
 export const environment = {
   production: false,
   version: versionLong,
   firebase: {
-    apiKey: "AIzaSyC9P18TqMSEKUMFuy-Lz6BfXVHOj5zSORo",
-    authDomain: "fir-87d91.firebaseapp.com",
-    databaseURL: "https://fir-87d91.firebaseio.com",
-    projectId: "fir-87d91",
-    storageBucket: "fir-87d91.appspot.com",
-    messagingSenderId: "847424067463",
-    appId: "1:847424067463:web:5594f49d6c4400a6"
+    apiKey: 'AIzaSyC9P18TqMSEKUMFuy-Lz6BfXVHOj5zSORo',
+    authDomain: 'fir-87d91.firebaseapp.com',
+    databaseURL: 'https://fir-87d91.firebaseio.com',
+    projectId: 'fir-87d91',
+    storageBucket: 'fir-87d91.appspot.com',
+    messagingSenderId: '847424067463',
+    appId: '1:847424067463:web:5594f49d6c4400a6'
   }
 };
 
