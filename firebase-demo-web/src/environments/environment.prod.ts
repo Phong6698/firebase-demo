3513a36604dc205a4467ae7b62d2184520200686
Chiramet Phong Penglerd <Phong@hotmail.ch>
2020-01-19 00:50:15 +0100

    environment

--- firebase-demo-web/src/environments/environment.prod.ts
+++ firebase-demo-web/src/environments/environment.prod.ts
@@ -4,12 +4,12 @@ export const environment = {
   production: true,
   version,
   firebase: {
-    apiKey: 'AIzaSyAJsJNEBcsnj-NvJT65hkW9oct33Y1kcDk',
-    authDomain: 'fir-dev-423cd.firebaseapp.com',
-    databaseURL: 'https://fir-dev-423cd.firebaseio.com',
-    projectId: 'fir-dev-423cd',
-    storageBucket: 'fir-dev-423cd.appspot.com',
-    messagingSenderId: '979297538852',
-    appId: '1:979297538852:web:d78f26a4739bb72a'
+    apiKey: 'AIzaSyC9P18TqMSEKUMFuy-Lz6BfXVHOj5zSORo',
+    authDomain: 'fir-87d91.firebaseapp.com',
+    databaseURL: 'https://fir-87d91.firebaseio.com',
+    projectId: 'fir-87d91',
+    storageBucket: 'fir-87d91.appspot.com',
+    messagingSenderId: '847424067463',
+    appId: '1:847424067463:web:5594f49d6c4400a6'
   }
 };
